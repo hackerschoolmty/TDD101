4095046072d7b8e36a78db624307fb97e6a75658
Yaser Almasri <yaserms@outlook.com>
2015-06-10 19:15:50 -0500

    Class 02

--- class02/fizzbuzz/lib/fizz_buzz.rb
+++ class02/fizzbuzz/lib/fizz_buzz.rb
@@ -1,4 +1,14 @@
 class FizzBuzz
   def initialize    
   end  
+
+  def self.play(number)
+    if (number % 3) == 0 && (number % 5) == 0
+      return 'FizzBuzz'
+    elsif number.to_s.include?('3') && number.to_s.include?('3')
+    end
+    return 'Fizz' if (number % 3) == 0 || number.to_s.include?('3')
+    return 'Buzz' if (number % 5) == 0 || number.to_s.include?('5')
+    number = number.to_s
+  end
 end
