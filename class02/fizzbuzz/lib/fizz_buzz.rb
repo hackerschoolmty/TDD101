a88d17bd82c36bf0aa8fb7ebf76a853387a63e38
Yaser Almasri <yaserms@outlook.com>
2015-06-10 19:23:18 -0500

    removes include 3 and 5

--- class02/fizzbuzz/lib/fizz_buzz.rb
+++ class02/fizzbuzz/lib/fizz_buzz.rb
@@ -5,7 +5,6 @@ class FizzBuzz
   def self.play(number)
     if (number % 3) == 0 && (number % 5) == 0
       return 'FizzBuzz'
-    elsif number.to_s.include?('3') && number.to_s.include?('3')
     end
     return 'Fizz' if (number % 3) == 0 || number.to_s.include?('3')
     return 'Buzz' if (number % 5) == 0 || number.to_s.include?('5')
